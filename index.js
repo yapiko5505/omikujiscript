15792e85ce73d4b10e28c5205e7d84ca4a65c4ea
yapiko <bloomingjewels7021@gmail.com>
2021-12-22 13:49:17 +0900

    omikuji1

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function drawkuji() {
+    let randomNumber = Math.floor(Math.random() * 4 + 1);
+    document.getElementsByClassName("kuji")[0].setAttribute('src', `images/omikuji${randomNumber}.png`);
+    document.querySelectorAll("button")[0].innerHTML= "もう一度引く";
+}
+
+
+
+
